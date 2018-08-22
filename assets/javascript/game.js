fc8c448f91262742493b50ff93d1b5d5a9b15ab2
Perri Specter <plspecter@gmail.com>
2018-08-22 11:38:32 -0400

    unite-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,21 @@
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
+var addedScore = 0;
+var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+var gemChosen = false;
+var firstGem = "";
 
+$(document).ready (function()  {
+
+$("#gem1").on("click", function() {
+console.log("this functions");
+if (gemChosen) {
+ firstGem = this.value;
+}
+  crystalValue.length = items[Math.floor(Math.random()* items.length)];
+  $("#totalScore").html(totalScore);
+
+});   
+
+});
